f43d9a796cf8a5b80397785440731899e23a584a
Sergiu <sergiu.buru99@gmail.com>
2023-03-02 12:28:09 -0600

    small z index change on cards and background

--- WorldSimplifiedNewsAppUi/src/Components/FeaturesCard/FeaturesCard.jsx
+++ WorldSimplifiedNewsAppUi/src/Components/FeaturesCard/FeaturesCard.jsx
@@ -7,7 +7,7 @@ function FeaturesCard({title, body, bgClass, animationX}) {
   const cardInView = useInView(cardRef);
 
     return (
-    <motion.div ref={cardRef} className={" rounded-md lg:py-7 md:py-2 md:px-2 lg:px-4 py-3 px-2 shadow-2xl md:w-4/5 md:h-5/6 h-95/100 w-11/12 text-white " + bgClass}
+    <motion.div ref={cardRef} className={"z-50 rounded-md lg:py-7 md:py-2 md:px-2 lg:px-4 py-3 px-2 shadow-2xl md:w-4/5 md:h-5/6 h-95/100 w-11/12 text-white " + bgClass}
         initial={{opacity: 0, x: animationX}} animate={cardInView ? {opacity: 1, x:0} : {opacity: 0}} transition={{duration: 1}}
     >
         <p className="xl:text-2xl md:text-xl text-sm font-rubik underline underline-offset-4">{title}</p>
