ce725b0f462bda8d8f46cc8f9ecfaa276b85a3e4
Sergiu <sergiu.buru99@gmail.com>
2023-03-02 15:05:17 -0600

    fix gradient

--- WorldSimplifiedNewsAppUi/src/Components/FeaturesCard/FeaturesCard.jsx
+++ WorldSimplifiedNewsAppUi/src/Components/FeaturesCard/FeaturesCard.jsx
@@ -7,7 +7,7 @@ function FeaturesCard({title, body, bgClass, animationX}) {
   const cardInView = useInView(cardRef);
 
     return (
-    <motion.div ref={cardRef} className={" mdy:p-10 rounded-md lg:py-7 md:py-2 md:px-2 lg:px-4 py-3 px-2 shadow-2xl md:w-4/5 md:h-9/10 h-95/100 w-11/12 text-white " + bgClass}
+    <motion.div ref={cardRef} className={" mdy:p-10 rounded-md lg:py-7 md:py-2 md:px-2 lg:px-4 py-3 px-2 shadow-2xl md:w-4/5 md:h-9/10 h-95/100 w-11/12 text-white bg-gradient-to-t from-indigo-600"}
         initial={{opacity: 0, x: animationX}} animate={cardInView ? {opacity: 1, x:0} : {opacity: 0}} transition={{duration: 1}}
     >
         <p className=" mdy:text-2xl xl:text-2xl md:text-xl text-sm font-rubik underline underline-offset-4">{title}</p>
